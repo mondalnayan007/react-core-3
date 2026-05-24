ad50eb378a2f904c259cf17254d27b1f8e4fa207
Nayan Mondal <n007kingkong@gmail.com>
2026-05-25 00:23:01 +0600

    creating product object taking value from user

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,20 @@ import './App.css'
 
 function App() {
 
-  const handleSubmit =()=>{
-    console.log("form is submitting");
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+  
+    const name  = e.target.name.value;
+    const price  = e.target.price.value;
+    const quantity  = e.target.quantity.value;
+
+    const newProduct = {
+      name,
+      price,
+      quantity
+    }
+
+    console.log(newProduct);
   }
 
 
@@ -16,8 +28,14 @@ function App() {
       <div>
 
         <form onSubmit={handleSubmit} action="">
-              <input type="text" placeholder='Enter  your name' />
-              <button type="submit"></button>
+              <input type="text" name='name' placeholder='Product name' /> 
+              <br />
+
+              <input type="text" name='price' placeholder='price' />
+              <br />
+              <input type="text" name='quantity' placeholder='Enter Quantity' />
+              <br />
+              <button type="submit">Submit</button>
         </form>
       </div>
 
